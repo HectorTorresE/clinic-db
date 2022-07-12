96ecd8f38cc55f204dc3b066f8ef31b7fee161c9
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-12 10:37:55 -0500

    Update repo

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -28,8 +28,12 @@ CREATE TABLE treatments (
   PRIMARY KEY (id) 
 );
 
+DROP TABLE IF EXISTS medical_histories_treatments; 
+CREATE TABLE medical_histories_treatments ( id INT GENERATED ALWAYS AS IDENTITY, medical_histories_id INT, treatments_id INT, PRIMARY KEY(id), CONSTRAINT FK_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id), CONSTRAINT FK_treatments_id FOREIGN KEY (treatments_id) REFERENCES treatments(id) ); 
+
+DROP TABLE IF EXISTS invoices; 
+CREATE TABLE invoices ( id INT GENERATED ALWAYS AS IDENTITY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT, PRIMARY KEY(id), CONSTRAINT FK_invoices_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id) ); 
 
 
-
-
-
+DROP TABLE IF EXISTS invoice_items; 
+CREATE TABLE invoice_items ( id INT GENERATED ALWAYS AS IDENTITY, unit_price DECIMAL, quantity INT, total_price DECIMAL, invoice_id INT, treatment_id INT, PRIMARY KEY(id), CONSTRAINT FK_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id), CONSTRAINT FK_invoice_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id) ); 
